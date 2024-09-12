f6e6874f81d6171b138d09e109183dac077a0ab6
Evilly Costa <152512061+EvillyCosta@users.noreply.github.com>
2024-09-12 14:03:26 -0300

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,65 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int tamanho = 0;
-    int x = 345, temp = x;
-    float res;
+int isPalindrome(int x) {
+    int tamanho= 0, temp = x;
+    int k, cont = 0;
 
-    while (temp != 0)
+    while (temp != 0) //poderia usar sizeof ao inves de achar o tamanho assim
     { // temporario pra preservar o valor de x
         temp = temp / 10;
         tamanho++;
-        printf("tamanho = %d\n", tamanho);
     }
-
-    if (x < 0) { //mudou valor de x para positivo
-        x = x*(-1);
-    }
-
-    printf("valor de x = %d\n", x);
-
-    int i = 0;
     float *digitos = (float *)malloc(tamanho * sizeof(float));
     float *digitos_invertidos = (float *) malloc (tamanho * sizeof(float));
-    int inteiro = 0;
-    float decimal = 0;
-    float div = (float)x;
 
-    while (i < tamanho)
-    {
-        res = div / 10;
-        inteiro = (int)res;
-        decimal = res - inteiro;
-
-        if (decimal < 1)
-        {
-            digitos[tamanho - 1] = decimal * 10;
-        }
-        digitos[i] = decimal * 10;
-        div = (float)inteiro;
-        i++;
-        printf("posição i = %d\n", i);
+    if (x < 0) { 
+        return 0;
     }
 
-    int k = tamanho;
-    while (k >= 0) {
-        // digitos_invertidos[k] = 
-
-    
-        k--;
-        printf("valor de k = %d", k);
+    int div = x;
+    for (int i = 0, k = tamanho - 1; i < tamanho && k >= 0; i++, k--) {
+        digitos[i] = div % 10; //recebe o ultimo digito
+        digitos_invertidos[k] = digitos[i]; 
+        div = div/10; //vai removendo o ultimo digito 
     }
-
     for (int j = 0; j < tamanho; j++)
     {
-        printf("%d, posicao = %d\n", (int)digitos[j], j);
+        if ((int)digitos_invertidos[j] == (int)digitos[j]) {
+            cont++;
+        }
     }
+
     free(digitos);
+    free(digitos_invertidos);
+
+    if (cont == tamanho) {
+        return 1;
+    }
 
     return 0;
+
 }
 
-// colocar criterio para quando for digitado valor negativo(testar *(-1)), criar segundo vetor
-// para inverter  o valor do primeiro e comparar se eh palindromo, transformar em função
+int main () {
+    int n = -121;
+
+    if (isPalindrome(n)) {
+        printf("eh palindromo");
+    } else {
+        printf("nao eh palindromo");
+    }
+}
